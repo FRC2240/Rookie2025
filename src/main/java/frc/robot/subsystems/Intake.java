1923b742bba16f8ddd5f1c7581e1fa3cdc1de071
DylanACollins <962253@scienceandtech.org>
2025-09-27 15:06:34 -0600

    Rookie work from 9/27

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -3,15 +3,25 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
+import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import com.revrobotics.ColorSensorV3;
 import com.revrobotics.ColorMatchResult;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.controls.TorqueCurrentFOC;
 import com.revrobotics.ColorMatch;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import static edu.wpi.first.units.Units.Amps;
+
+import java.util.Optional;
+
+import edu.wpi.first.wpilibj.DriverStation;
 
 public class Intake extends SubsystemBase {
   private final I2C.Port i2cPort = I2C.Port.kOnboard;
@@ -22,6 +32,23 @@ public class Intake extends SubsystemBase {
   private final Color kGreenTarget = new Color(0.197, 0.561, 0.240);
   private final Color kRedTarget = new Color(0.561, 0.232, 0.114);
   private final Color kYellowTarget = new Color(0.361, 0.524, 0.113);
+
+  // motors
+  TalonFX m_pivotMotor = new TalonFX(31);
+  TalonFX m_rollerMotor = new TalonFX(5);
+  
+  // Intake states
+  private enum State {
+    IDLE,
+    ACTIVE,
+    EJECTING,
+    INTAKING
+  }
+
+  State m_state = State.IDLE;
+
+  String m_colorString;
+  
 
   /** Creates a new Intake subsystem. */
   public Intake() {
@@ -44,6 +71,22 @@ public class Intake extends SubsystemBase {
           /* one-time action goes here */
         });
   }
+
+public Command toggleIntake() {
+  return Commands.runOnce(() -> {
+    Current current;
+    if (m_state == State.IDLE) {
+      current = Amps.of(3.0);
+      m_state = State.ACTIVE;
+ } else {
+      current = Amps.of(0.0);
+      m_state = State.IDLE;
+    }
+    TorqueCurrentFOC req = new TorqueCurrentFOC(current);
+    m_rollerMotor.setControl(req);
+  });
+}
+
 
   /**
    * An example method querying a boolean state of the subsystem (for example, a
@@ -68,26 +111,69 @@ public class Intake extends SubsystemBase {
     ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
 
     if (match.color == kBlueTarget) {
-      colorString = "Blue";
+      m_colorString = "Blue";
     } else if (match.color == kRedTarget) {
-      colorString = "Red";
+      m_colorString = "Red";
     } else if (match.color == kGreenTarget) {
-      colorString = "Green";
+      m_colorString = "Green";
     } else if (match.color == kYellowTarget) {
-      colorString = "Yellow";
+      m_colorString = "Yellow";
     } else {
-      colorString = "Unknown";
+      m_colorString = "Unknown";
     }
 
     SmartDashboard.putNumber("Red", detectedColor.red);
     SmartDashboard.putNumber("Green", detectedColor.green);
     SmartDashboard.putNumber("Blue", detectedColor.blue);
     SmartDashboard.putNumber("Confidence", match.confidence);
-    SmartDashboard.putString("Detected Color", colorString);
+    SmartDashboard.putString("Detected Color", m_colorString);
+    SmartDashboard.putBoolean("IsCorrect", isCorrect());
+    SmartDashboard.putBoolean("IsWrong", isWrong());
   }
 
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
   }
+
+boolean isCorrect(){
+
+Optional <DriverStation.Alliance> aColor = DriverStation.getAlliance();
+
+  if (!aColor.isPresent()) {
+    return false; 
+  } 
+  if ((m_colorString == "Red") && (aColor.get() == DriverStation.Alliance.Red)) 
+  {
+    return true;
+  } 
+  if ((m_colorString == "Blue") && (aColor.get() == DriverStation.Alliance.Blue))
+   {
+    return true;
+  }
+ return false;
+} 
+
+boolean isWrong(){
+
+Optional <DriverStation.Alliance> aColor = DriverStation.getAlliance();
+
+  if (!aColor.isPresent()) {
+    return false;
+  }
+  if ((m_colorString == "Red") && (aColor.get() == DriverStation.Alliance.Blue)) 
+  {
+    return true;
+  }
+  if ((m_colorString == "Blue") && (aColor.get() == DriverStation.Alliance.Red)) 
+  {
+    return true;
+  }
+  return false;
+} 
+
+
+
+
+
 }
